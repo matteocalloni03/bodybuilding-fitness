625ee1668b3e2179147b0b158d76fe62dee1971e
matteocalloni03 <matteocalloni03@gmail.com>
2024-06-17 14:04:22 +0200

    giorni activity + bottone click

--- app/src/main/java/com/example/bodybuildingfitness/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/example/bodybuildingfitness/ui/slideshow/SlideshowFragment.kt
@@ -1,12 +1,15 @@
 package com.example.bodybuildingfitness.ui.slideshow
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.bodybuildingfitness.R
 import com.example.bodybuildingfitness.databinding.FragmentSlideshowBinding
 
 class SlideshowFragment : Fragment() {
@@ -34,6 +37,42 @@ class SlideshowFragment : Fragment() {
         }
         return root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //giorno 1
+        val button1 = binding.button1
+        button1.setOnClickListener {
+            val intent = Intent(requireContext(), giorni1::class.java)
+            startActivity(intent)
+        }
+        //giorno 2
+        val button2 = binding.button2
+        button2.setOnClickListener {
+            val intent = Intent(requireContext(), giorni2::class.java)
+            startActivity(intent)
+        }
+        //giorno 3
+        val button3 = binding.button3
+        button3.setOnClickListener {
+            val intent = Intent(requireContext(), giorni3::class.java)
+            startActivity(intent)
+        }
+        //giorno 4
+        val button4 = binding.button4
+        button4.setOnClickListener {
+            val intent = Intent(requireContext(), giorni4::class.java)
+            startActivity(intent)
+        }
+        //giorno 5
+        val button5 = binding.button5
+        button5.setOnClickListener {
+            val intent = Intent(requireContext(), giorni1::class.java)
+            startActivity(intent)
+        }
+
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
