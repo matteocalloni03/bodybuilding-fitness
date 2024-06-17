a4ad93c0efea3b08db3219c2d11d458f6afb79d8
matteocalloni03 <matteocalloni03@gmail.com>
2024-06-17 14:47:03 +0200

    calendario al giorno d'oggi

--- app/src/main/java/com/example/bodybuildingfitness/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/bodybuildingfitness/ui/gallery/GalleryFragment.kt
@@ -1,12 +1,15 @@
 package com.example.bodybuildingfitness.ui.gallery
 
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.bodybuildingfitness.R
 import com.example.bodybuildingfitness.databinding.FragmentGalleryBinding
 
 class GalleryFragment : Fragment() {
@@ -28,11 +31,16 @@ class GalleryFragment : Fragment() {
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val calendarView = view.findViewById<CalendarView>(R.id.calendarView)
+
+        // Imposta la data di oggi
+        val oggi = Calendar.getInstance()
+        calendarView.date = oggi.timeInMillis
     }
 
     override fun onDestroyView() {
