a98702372eb9c8e817938d42a87dd629839cacd6
matteocalloni03 <matteocalloni03@gmail.com>
2024-06-19 17:26:36 +0200

    aggiunta altezza a corpo

--- app/src/main/java/com/example/bodybuildingfitness/ui/corpo/corpoFragment.kt
+++ app/src/main/java/com/example/bodybuildingfitness/ui/corpo/corpoFragment.kt
@@ -18,7 +18,6 @@ import com.example.bodybuildingfitness.databinding.FragmentCorpoBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
-
 class corpoFragment : Fragment() {
 
     private var _binding: FragmentCorpoBinding? = null
@@ -50,11 +49,14 @@ class corpoFragment : Fragment() {
         val imageView1 = view.findViewById<ImageView>(R.id.imageuomo)
         val imageView2 = view.findViewById<ImageView>(R.id.imagedonna)
         val editTextNumber = view.findViewById<EditText>(R.id.editTextNumber)
+        val editTextHeight = view.findViewById<EditText>(R.id.editTextHeight)
         val textpeso = view.findViewById<TextView>(R.id.textpeso)
+        val textAltezza = view.findViewById<TextView>(R.id.textAltezza)
         val saveButton = view.findViewById<Button>(R.id.saveButton)
 
-        // Pulire il campo EditText all'avvio
+        // Pulire i campi EditText all'avvio
         editTextNumber.text.clear()
+        editTextHeight.text.clear()
 
         radioGroup.setOnCheckedChangeListener { group, checkedId ->
             when (checkedId) {
@@ -71,53 +73,60 @@ class corpoFragment : Fragment() {
 
         saveButton.setOnClickListener {
             val peso = editTextNumber.text.toString()
+            val altezza = editTextHeight.text.toString()
 
-            if (peso.isNotEmpty()) {
+            if (peso.isNotEmpty() && altezza.isNotEmpty()) {
                 val user = auth.currentUser
 
                 if (user != null) {
-                    val pesoData = hashMapOf(
-                        "peso" to peso
+                    val data = hashMapOf(
+                        "peso" to peso,
+                        "altezza" to altezza
                     )
 
                     db.collection("users").document(user.uid)
-                        .set(pesoData)
+                        .set(data)
                         .addOnSuccessListener {
-                            Log.d("Firebase", "Peso salvato con successo")
-                            Toast.makeText(requireContext(), "Peso salvato!", Toast.LENGTH_SHORT).show()
+                            Log.d("Firebase", "Peso e altezza salvati con successo")
+                            Toast.makeText(requireContext(), "Peso e altezza salvati!", Toast.LENGTH_SHORT).show()
                             textpeso.text = peso
+                            textAltezza.text = altezza
                             editTextNumber.text.clear() // Pulisce il campo EditText dopo il salvataggio
+                            editTextHeight.text.clear() // Pulisce il campo EditText dopo il salvataggio
                         }
                         .addOnFailureListener { e ->
-                            Log.w("Firebase", "Errore nel salvataggio del peso", e)
-                            Toast.makeText(requireContext(), "Errore nel salvataggio del peso", Toast.LENGTH_SHORT).show()
+                            Log.w("Firebase", "Errore nel salvataggio del peso e altezza", e)
+                            Toast.makeText(requireContext(), "Errore nel salvataggio del peso e altezza", Toast.LENGTH_SHORT).show()
                         }
                 } else {
                     Log.w("Firebase", "Utente non autenticato")
                     Toast.makeText(requireContext(), "Utente non autenticato", Toast.LENGTH_SHORT).show()
                 }
             } else {
-                Toast.makeText(requireContext(), "Inserisci il tuo peso", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "Inserisci il tuo peso e altezza", Toast.LENGTH_SHORT).show()
             }
         }
 
-        // Recupera il peso salvato da Firebase quando la vista viene creata
+        // Recupera peso e altezza salvati da Firebase quando la vista viene creata
         val user = auth.currentUser
         if (user != null) {
             db.collection("users").document(user.uid).get()
                 .addOnSuccessListener { document ->
                     if (document != null && document.exists()) {
                         val savedPeso = document.getString("peso")
+                        val savedAltezza = document.getString("altezza")
                         savedPeso?.let {
                             textpeso.text = it
-
+                        }
+                        savedAltezza?.let {
+                            textAltezza.text = it
                         }
                     } else {
                         Log.d("Firebase", "Nessun documento trovato")
                     }
                 }
                 .addOnFailureListener { e ->
-                    Log.w("Firebase", "Errore nel recupero del peso", e)
+                    Log.w("Firebase", "Errore nel recupero del peso e altezza", e)
                 }
         }
     }
@@ -127,6 +136,7 @@ class corpoFragment : Fragment() {
         _binding = null
     }
 }
+
 
 
 
